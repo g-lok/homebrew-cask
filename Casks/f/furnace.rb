a1f7a702d09a11f43156ba34760adc66aeecd36d
Garrett Anderson <g@devnull.rip>
2025-11-22 22:15:17 -0800

    Shorten furnace verified url

--- Casks/f/furnace.rb
+++ Casks/f/furnace.rb
@@ -5,7 +5,7 @@ cask "furnace" do
   sha256 arm: "3f2a925911c5e8babaedbee891bfda669305930a024af95b83a1103e55535755",
         intel: "f901c98d96b89d68bd84e4df9e73b2e16e6566ddc75c91ec24cc1dd09300f4ae"
   url "https://github.com/tildearrow/furnace/releases/download/v{version}/furnace-{version}-mac-{arch}.dmg",
-    verified: "github.com/tildearrow/furnace/releases/"
+    verified: "github.com/tildearrow/"
 
   name "Furnace"
   desc "A multi-system chiptune tracker compatible with DefleMask modules."
