cb912137ae0d77df2fc0da5f34181518fef39242
Garrett Anderson <g@devnull.rip>
2025-11-22 22:29:43 -0800

    Fix ruby typo

--- Casks/f/furnace.rb
+++ Casks/f/furnace.rb
@@ -4,7 +4,7 @@ cask "furnace" do
   version "0.6.8.3"
   sha256 arm: "3f2a925911c5e8babaedbee891bfda669305930a024af95b83a1103e55535755",
         intel: "f901c98d96b89d68bd84e4df9e73b2e16e6566ddc75c91ec24cc1dd09300f4ae"
-  url "https://github.com/tildearrow/furnace/releases/download/v{version}/furnace-{version}-mac-{arch}.dmg",
+  url "https://github.com/tildearrow/furnace/releases/download/v#{version}/furnace-#{version}-mac-#{arch}.dmg",
     verified: "github.com/tildearrow/"
 
   name "Furnace"
